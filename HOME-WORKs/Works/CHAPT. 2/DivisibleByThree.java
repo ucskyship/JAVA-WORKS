8930d63ecddcf1ecf15d296138aabd7ae3355cfa
UZOCHUKWU CHINEMEREM <chat.with.ucjhay@gmail.com>
2022-04-21 04:56:42 +0100

    This is actually Chapter2 Files

---
+++ HOME-WORKs/Works/CHAPT. 2/DivisibleByThree.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class DivisibleByThree {
+    public static void main(String... java){
+
+        Scanner Input = new Scanner(System.in);
+
+        System.out.print("Enter any Number: ");
+        int number = Input.nextInt();
+
+        if (number % 3 == 0) {
+            System.out.printf("The number %d is divisible by 3", number);
+        }
+        if (number % 3 != 0) {
+            System.out.printf("The number %d is not divisible by 3", number);
+        }
+    }
+}
---
+++ HOME-WORKs/Works/CHAPT. 2/SeparatingDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class SeparatingDigits {
+    public static void main(String... args){
+
+        Scanner Input = new Scanner(System.in);
+
+        System.out.print("Enter a Five digit number: ");
+        int fiveDigits = Input.nextInt();
+        char a = Input.next().charAt(0);
+        char b = Input.next().charAt(1);
+        char c = Input.next().charAt(2);
+        char d = Input.next().charAt(3);
+        char e = Input.next().charAt(4);
+
+        System.out.printf("%c   %c   %c   %c   %c", a, b, c, d, e);
+    }
+
+}
