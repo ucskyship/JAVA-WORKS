8baa44c99ab79feb37dcc51721de90859065d3f5
UZOCHUKWU CHINEMEREM <chat.with.ucjhay@gmail.com>
2022-04-29 16:19:45 +0100

    Test file

--- TEST/Welcome3.java
+++ TEST/Welcome3.java
@@ -2,7 +2,7 @@ public class Welcome3 {
 
 	public static void main(String... args){
 	
-		System.out.printf("Welcome\nto\nJava\nProgramming");
+		System.out.print("Welcome\nto\nJava\nProgramming");
 	}
 
 
